e33f2b5a7c4036e785fd11fce18db6247f9c8f36
Logan Gouget <logan.gouget@gmail.com>
2023-07-04 15:36:04 +0200

    fix: update api doc

--- services/table-manager/src/main.ts
+++ services/table-manager/src/main.ts
@@ -6,10 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Table manager')
+    .setDescription('An API to manage tables in a restaurant')
     .setVersion('1.0')
-    .addTag('cats')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
