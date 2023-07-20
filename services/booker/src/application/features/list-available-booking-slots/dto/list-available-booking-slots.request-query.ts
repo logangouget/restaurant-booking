5b843f72ed3bfa9b3123169bbf807f40e6b2f121
Logan Gouget <logan.gouget@gmail.com>
2023-07-20 14:54:30 +0200

    feat: add dateRange validation
    add dateRange validation to GET /booking-slots endpoint

--- services/booker/src/application/features/list-available-booking-slots/dto/list-available-booking-slots.request-query.ts
+++ services/booker/src/application/features/list-available-booking-slots/dto/list-available-booking-slots.request-query.ts
@@ -1,6 +1,36 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsDateString, IsInt, Min } from 'class-validator';
 import { Transform } from 'class-transformer';
+import {
+  IsDateString,
+  IsInt,
+  Min,
+  Validate,
+  ValidationArguments,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
+
+@ValidatorConstraint({ name: 'dateRange', async: false })
+export class DateRange implements ValidatorConstraintInterface {
+  validate(endDate: string, args: ValidationArguments) {
+    const startDate = args.object['startDate'] as string;
+
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+    const diff = Math.abs(end.getTime() - start.getTime());
+    const diffDays = Math.ceil(diff / (1000 * 3600 * 24));
+
+    if (diffDays > 14) {
+      return false;
+    }
+
+    return true;
+  }
+
+  defaultMessage() {
+    return 'Date ($value) is not in range! (14 days)';
+  }
+}
 
 export class ListAvailableBookingSlotsRequestQuery {
   @IsDateString()
@@ -15,6 +45,7 @@ export class ListAvailableBookingSlotsRequestQuery {
     description: 'The end date of the booking slot',
     example: '2021-01-01',
   })
+  @Validate(DateRange)
   endDate: string;
 
   @IsInt()
